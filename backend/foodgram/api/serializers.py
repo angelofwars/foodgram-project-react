9b82827265dd436f83f965b94f1696edb8ad75cc
angelofwars <angelofwars59@gmail.com>
2023-05-27 19:35:34 +0500

    create serialize and viewset

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,9 +1,35 @@
-from djoser.serializers import UserSerializer
+from django.conf import settings
+from django.db import models, transaction
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework import (exceptions, fields, relations, serializers, status,
+                            validators)
 
-from ..user.models import User
+from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
+                            ShoppingCart, Tag)
+from user.models import Follow, User
 
 
-class CustomUserSerializer(UserSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для авторизованных и 
+    не аворизованных пользователей."""
+
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name')
+        fields = (
+            "username", "email", "first_name", "last_name", "bio", "role"
+        )
+    
+    def get_is_subscribed(self, author):
+        """Проверка подписки пользователей."""
+        request = self.context.get("request")
+        return (request and request.user.is_authenticated
+                and request.user.follower.filter(author=author).exists())
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """ Сериализатор для модели Tag."""
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action, api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from reviews.models import Category, Genre, Review, Title, User
+from users.models import Follow, User
 
 from .permissions import (IsAdmin, IsAdminOrStaffPermission,
                           IsAuthorOrReadOnly, IsModerator,
@@ -22,16 +22,16 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = (IsAdminOrStaffPermission,)
     filter_backends = (SearchFilter,)
-    search_fields = ('=username',)
-    lookup_field = 'username'
+    search_fields = ("=username",)
+    lookup_field = "username"
 
     @action(
         detail=False,
-        methods=['GET', 'PATCH'],
+        methods=["GET", "PATCH"],
         permission_classes=(IsUserForSelfPermission,)
     )
     def me(self, request):
-        if request.method == 'PATCH':
+        if request.method == "PATCH":
             serializer = UserSerializer(
                 request.user, data=request.data, partial=True
             )
@@ -42,7 +42,7 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None, **kwargs):
-        if 'partial' in kwargs:
+        if "partial" in kwargs:
             instance = self.get_object()
             serializer = UserSerializer(
                 instance, data=request.data, partial=True
@@ -53,41 +53,52 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-    @api_view(['POST'])
-    def users_signup(request):
-        username = request.data.get('username')
-        if not User.objects.filter(username=username).exists():
-            serializer = AuthSignUpSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            send_confirmation_code(username)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        user = get_object_or_404(User, username=username)
-        serializer = AuthSignUpSerializer(
-            user, data=request.data, partial=True
-        )
+@api_view(["POST"])
+def users_signup(request):
+    username = request.data.get("username")
+    if not User.objects.filter(username=username).exists():
+        serializer = AuthSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         send_confirmation_code(username)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    user = get_object_or_404(User, username=username)
+    serializer = AuthSignUpSerializer(
+        user, data=request.data, partial=True
+    )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    send_confirmation_code(username)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-    @api_view(['POST'])
-    def users_get_token(request):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        username = serializer.validated_data['username']
-        confirmation_code = serializer.validated_data['confirmation_code']
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            return Response(
-                'Пользователь не найден', status=status.HTTP_404_NOT_FOUND
-            )
-        if user.confirmation_code == confirmation_code:
-            refresh = RefreshToken.for_user(user)
-            token_data = {'token': str(refresh.access_token)}
-            return Response(token_data, status=status.HTTP_200_OK)
+@api_view(["POST"])
+def users_get_token(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data["username"]
+    confirmation_code = serializer.validated_data["confirmation_code"]
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
         return Response(
-            'Код подтверждения неверный', status=status.HTTP_400_BAD_REQUEST
+            "Пользователь не найден", status=status.HTTP_404_NOT_FOUND
         )
+    if user.confirmation_code == confirmation_code:
+        refresh = RefreshToken.for_user(user)
+        token_data = {"token": str(refresh.access_token)}
+        return Response(token_data, status=status.HTTP_200_OK)
+    return Response(
+        "Код подтверждения неверный", status=status.HTTP_400_BAD_REQUEST
+    )
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для отображения тегов."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (permissions.AllowAny,)
+    pagination_class = None
+
+    
