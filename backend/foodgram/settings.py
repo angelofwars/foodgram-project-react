48c33c5d6d5f835be88ee362d898ebc46838df40
angelofwars <angelofwars59@gmail.com>
2023-06-07 21:36:50 +0500

    deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -189,7 +189,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Your project description",
     "VERSION": '1.0.0',
     "SERVE_INCLUDE_SCHEMA": False,
-    # OTHER SETTINGS
+
 }
 
 # Далее вынесены постоянные которые нужны для работы проекта
